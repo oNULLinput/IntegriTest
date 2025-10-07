b2297d3cee9a802d09787f78da8d6619597ace61
Delica, Riu L. <delica.riuict@gmail.com>
2025-10-07 13:45:11 +0800

    refactor: simplify jest configuration by removing unused settings

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
-  testMatch: ["<rootDir>/tests/**/*.test.js", "<rootDir>/tests/**/*.spec.js"],
-  collectCoverageFrom: ["public/**/*.js", "!public/**/*.min.js", "!public/**/node_modules/**"],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
-  moduleNameMapping: {  // <- CHANGE THIS LINE
-    "^@/(.*)$": "<rootDir>/public/$1",
-  },
-  globals: {
-    window: {},
-    document: {},
-    localStorage: {},
-    sessionStorage: {},
-  },
+  testEnvironment: "jsdom"
 }
