05357e1bb619b2e71c7b99daf3565a016697a292
Delica, Riu L. <delica.riuict@gmail.com>
2025-10-07 14:40:51 +0800

    refactor: remove unused setupFilesAfterEnv configuration from jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,3 @@
 module.exports = {
   testEnvironment: "jsdom",
-  // setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],  // ← REMOVE THIS LINE
-  testMatch: ["<rootDir>/tests/**/*.test.js", "<rootDir>/tests/**/*.spec.js"],
-  collectCoverageFrom: ["public/**/*.js", "!public/**/*.min.js", "!public/**/node_modules/**"],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov", "html"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/public/$1",
-  },
-  globals: {
-    window: {},
-    document: {},
-    localStorage: {},
-    sessionStorage: {},
-  },
 }
